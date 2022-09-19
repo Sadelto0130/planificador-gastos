e9488171803c32c2ad4363949fcdb4366ee007fa
sadelto0130 <vsalaz00@live.com>
2022-09-19 02:24:32 -0300

    App finalizada, se puede agregar boton para modificar presupuesto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({
+  presupuesto, 
+  setPresupuesto,    
+  setIsValidPresupuesto
+}) => {
+
+  const [mensaje, setMensaje] = useState('');
+
+  const handlePresupuesto = (e) =>{
+    e.preventDefault();
+
+    if(!(presupuesto) || presupuesto < 0){
+      setMensaje('No es un presuepuesto valido')
+
+      return
+    } 
+
+    setMensaje('')
+    setIsValidPresupuesto(true);
+
+    console.log(presupuesto)
+  }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
 
-        <form className="formulario">
+        <form onSubmit={handlePresupuesto} className="formulario">
             <div className='campo'>
                 <label>Definir Presupuesto</label>
 
                 <input 
                     className='nuevo-presupuesto'
-                    type="text"
+                    type="number"
                     placeholder="Añade tu Presupuesto"
+                    value={presupuesto}
+                    onChange = {e => setPresupuesto(Number(e.target.value))}
                 />
             </div>
 
             <input type="submit" value="Añadir"/>
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
         
     </div>
